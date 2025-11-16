756063742d7530ce7a533c9ac1716393ac6f6256
Muhammad Alfian <me.alfians29@gmail.com>
2025-11-16 11:03:31 +0700

    Menu and Navbar design

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 import Menu from '@/components/Menu';
+import Navbar from '@/components/Navbar';
 
 export default function DashboardLayout({
   children,
@@ -11,10 +12,10 @@ export default function DashboardLayout({
   return (
     <div className='h-screen flex'>
       {/* LEFT SIDE */}
-      <div className='w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200'>
+      <div className='w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4'>
         <Link
           href='/'
-          className='flex items-center justify-center lg:justify-start gap-2 p-4'
+          className='flex items-center justify-center lg:justify-start gap-2'
         >
           <Image src='/logo.png' alt='logo' width={32} height={32} />
           <span className='hidden lg:block'>Schoolify</span>
@@ -22,8 +23,9 @@ export default function DashboardLayout({
         <Menu />
       </div>
       {/* RIGHT SIDE */}
-      <div className='w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-green-200'>
-        R
+      <div className='w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll'>
+        <Navbar />
+        {children}
       </div>
     </div>
   );
