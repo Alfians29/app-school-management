117ade213a3a801ecb0789f0c200242d1e0d62d4
Muhammad Alfian <me.alfians29@gmail.com>
2025-12-03 17:34:08 +0700

    activasion search feature for TeacherPage

--- src/components/TableSearch.tsx
+++ src/components/TableSearch.tsx
@@ -1,15 +1,33 @@
-import Image from 'next/image';
+"use client";
+
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 const TableSearch = () => {
+  const router = useRouter();
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const value = (e.currentTarget[0] as HTMLInputElement).value;
+
+    const params = new URLSearchParams(window.location.search);
+    params.set("search", value);
+    router.push(`${window.location.pathname}?${params}`);
+  };
+
   return (
-    <div className='w-full md:w-auto flex items-center text-xs gap-2 rounded-full ring-[1.5px] ring-gray-300 px-2'>
-      <Image src='/search.png' alt='' width={14} height={14} />
+    <form
+      onSubmit={handleSubmit}
+      className="w-full md:w-auto flex items-center text-xs gap-2 rounded-full ring-[1.5px] ring-gray-300 px-2"
+    >
+      <Image src="/search.png" alt="" width={14} height={14} />
       <input
-        type='text'
-        placeholder='Search...'
-        className='w-[200px] p-2 bg-transparent outline-none'
+        type="text"
+        placeholder="Search..."
+        className="w-[200px] p-2 bg-transparent outline-none"
       />
-    </div>
+    </form>
   );
 };
 
